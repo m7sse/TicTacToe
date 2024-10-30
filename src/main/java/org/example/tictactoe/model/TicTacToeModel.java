6b2a056c2bf5df0235dda70cc8b8bbd2fc1da773
m7sse <muslim.yaraliev@iths.se>
2024-10-30 10:37:07 +0100

    lite nya metoder, men ska testa en annan approach  kodvis

--- src/main/java/org/example/tictactoe/model/TicTacToeModel.java
+++ src/main/java/org/example/tictactoe/model/TicTacToeModel.java
@@ -10,15 +10,33 @@ public class TicTacToeModel {
         initializeBoard();
     }
 
-    private void initializeBoard(){
+    private void initializeBoard() {
         for (int i = 0; i < 3; i++) {
             Arrays.fill(board[i], ' ');
         }
     }
 
-    public void makeMove(int row, int col){
-        board[row][col] = player;
-        player = (player == 'X') ? 'O' : 'X';
+    public boolean isValidMove(int row, int col) {
+        return row >= 0 && row < 3 && col >= 0 && col < 3;
     }
+
+    public void makeMove(int row, int col) {
+        if (isValidMove(row, col)) {
+            board[row][col] = player;
+            player = (player == 'X') ? 'O' : 'X';
+        }
+    }
+
+    public boolean checkWinner() {
+        for (int i = 0; i < 3; i++) {
+            if ((board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') ||
+                    (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ')) {
+                return true;
+            }
+        }
+        return (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') ||
+                (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ');
+    }
+
 }
 
