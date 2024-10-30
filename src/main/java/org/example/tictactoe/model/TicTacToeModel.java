7a8a2e1b6f99788dd9a4aded0ed0fe254b91eae6
m7sse <muslim.yaraliev@iths.se>
2024-10-30 10:43:45 +0100

    början på andra approachen, ser nästan likadant ut men få ändringar och enligt mig lite finare skrivet

--- src/main/java/org/example/tictactoe/model/TicTacToeModel.java
+++ src/main/java/org/example/tictactoe/model/TicTacToeModel.java
@@ -3,39 +3,25 @@ package org.example.tictactoe.model;
 import java.util.Arrays;
 
 public class TicTacToeModel {
-    char[][] board = new char[3][3];
-    private char player = 'X';
+    private char[][] board;
+    private char player;
 
     public TicTacToeModel() {
+        board = new char[3][3];
+        player = 'X';
         initializeBoard();
     }
 
-    private void initializeBoard() {
-        for (int i = 0; i < 3; i++) {
-            Arrays.fill(board[i], ' ');
-        }
-    }
+    private void initializeBoard(){
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = ' ';
 
-    public boolean isValidMove(int row, int col) {
-        return row >= 0 && row < 3 && col >= 0 && col < 3;
-    }
-
-    public void makeMove(int row, int col) {
-        if (isValidMove(row, col)) {
-            board[row][col] = player;
-            player = (player == 'X') ? 'O' : 'X';
-        }
-    }
-
-    public boolean checkWinner() {
-        for (int i = 0; i < 3; i++) {
-            if ((board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') ||
-                    (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ')) {
-                return true;
             }
         }
-        return (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') ||
-                (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ');
+    }
+    private boolean isValidMove(int row, int col){
+        return row >= 0 && col >= 0 && row < 3 && col < 3 && board[row][col] == ' ';
     }
 
 }
