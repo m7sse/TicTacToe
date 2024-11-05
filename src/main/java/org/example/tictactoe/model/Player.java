e8619770cc5dca361afba3fc8a77a12fe635980d
m7sse <muslim.yaraliev@iths.se>
2024-11-05 09:21:42 +0100

    Player klassen, färdig

--- src/main/java/org/example/tictactoe/model/Player.java
+++ src/main/java/org/example/tictactoe/model/Player.java
@@ -1,5 +1,19 @@
 package org.example.tictactoe.model;
 
 public class Player {
+    private char symbol;
 
+    public Player(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public void makeMove(TicTacToeModel model, int row, int col) {
+        if (model.isValidMove(row, col)) {
+            model.makeMove(row, col);
+        }
+    }
 }
