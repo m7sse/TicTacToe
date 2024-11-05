89e3c2f59dbe0622b1769a46bcb335b513aa0793
m7sse <muslim.yaraliev@iths.se>
2024-11-05 09:23:23 +0100

    Börjat syssla med Computer klassen

--- src/main/java/org/example/tictactoe/model/Computer.java
+++ src/main/java/org/example/tictactoe/model/Computer.java
@@ -1,4 +1,13 @@
 package org.example.tictactoe.model;
 
+import java.util.Random;
+
 public class Computer {
+    private char symbol;
+    private Random random = new Random();
+
+    public Computer(char symbol) {
+        this.symbol = symbol;
+    }
+
 }
